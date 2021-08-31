4f4c3889eab94886a56eab76cb164a1cae08ae2e
Christine Beardsworth <christine.beardsworth@nioz.nl>
2021-08-31 13:58:33 +0200

    tidy script and change where speed was calculated

--- R/fun_make_res_patch_speed.R
+++ R/fun_make_res_patch_speed.R
@@ -63,55 +63,49 @@
 #' @export
 #'
 atl_res_patch_speed <- function(data,
-                          max_speed = 10,
-                          lim_spat_indep = 100,
-                          lim_time_indep = 30,
-                          min_fixes = 3,
-                          summary_variables = c(),
-                          summary_functions = c()) {
+                                max_speed = 10,
+                                lim_spat_indep = 100,
+                                lim_time_indep = 30,
+                                min_fixes = 3,
+                                summary_variables = c(),
+                                summary_functions = c()) {
   area <- disp_in_patch <- NULL
   dist_bw_patch <- dist_in_patch <- duration <- NULL
   id <- newpatch <- nfixes <- patch <- NULL
   patchdata <- polygons <- spat_diff <- NULL
   time <- time_diff <- time_end <- time_start <- NULL
   x_end <- x_start <- y_end <- y_start <- NULL
-
+  
   # check data is a data.frame and has a resTime column
   # check if data frame
   assertthat::assert_that(is.data.frame(data),
-    msg = glue::glue("getResPatch: input not a \\
+                          msg = glue::glue("getResPatch: input not a \\
                           dataframe object, \\
                           has class {stringr::str_flatten(class(data),
-                                           collapse = ' ')}!")
-  )
-
-  assertthat::assert_that(min(c(
-    max_speed, lim_spat_indep,
-    lim_time_indep, min_fixes
-  )) > 0,
-  msg = "atl_make_res_patch: function needs \\
-                          positive arguments"
-  )
-
+                                           collapse = ' ')}!"))
+  
+  assertthat::assert_that(min(c(max_speed, lim_spat_indep,lim_time_indep, min_fixes)) > 0,
+                          msg = "atl_make_res_patch: function needs \\
+                          positive arguments")
+  
   # convert variable units from minutes to seconds
   lim_time_indep <- lim_time_indep * 60
-
+  
   # get names and numeric variables
-  names_req <- c("id", "x", "y", "time", summary_variables)
-
+  names_req <- c("id", "x", "y", "time")
+  
   # include asserts checking for required columns
   atl_check_data(data, names_expected = names_req)
-
+  
   # make datatable to use functions
   if (!is.data.table(data)) {
     data.table::setDT(data)
   }
-
+  
   # sort by time
   data.table::setorderv(data, time)
   assertthat::assert_that(min(diff(data$time)) >= 0,
-    msg = "data for segmentation is not ordered by time"
-  )
+                          msg = "data for segmentation is not ordered by time")
   tryCatch(
     expr = {
       
@@ -122,32 +116,35 @@ atl_res_patch_speed <- function(data,
           data = data,
           x = "x", y = "y"
         ),
-        time_diff = c(Inf, as.numeric(diff(time))),
-        speed = spat_diff/time_diff
+        time_diff = c(Inf, as.numeric(diff(time)))
       )]
-
+      
       # first spatial difference is infinity for calculation purposes
       data[1, c("spat_diff")] <- Inf
-
+      
+      
+      data[, `:=`(speed = spat_diff/time_diff)]
+      
+      
       # merge points if not spatially independent
       # compare distance from previous point to buffer_radius
-      data <- data[, patch := cumsum(speed > max_speed | spat_diff > lim_spat_indep |
-        time_diff > lim_time_indep)]
-
+      data[, patch := cumsum(speed > max_speed | spat_diff > lim_spat_indep |
+                               time_diff > lim_time_indep)]
+      
       # count fixes and patch and remove small patches
       # count number of points per patch
-      data <- data[, nfixes := .N, by = c("id", "patch")]
-
+      data[, nfixes := .N, by = c("id", "patch")]
+      
       # remove patches with 2 or fewer points
-      data <- data[nfixes >= min_fixes, ]
+      data <- data[nfixes >= min_fixes]
       data[, nfixes := NULL]
-
+      
       # get time mean and extreme points for spatio-temporal independence calc
       # nest data
-      data <- data[, list(list(.SD)), by = list(id, patch)]
+      data <- data[, list(list(.SD)), by = list(id, patch)] # nests data for id and patch
       setnames(data, old = "V1", new = "patchdata")
       data[, nfixes := as.integer(lapply(patchdata, nrow))]
-
+      
       # summarise mean, first and last
       data[, patch_summary := lapply(patchdata, function(dt) {
         dt2 <- dt[, unlist(lapply(.SD, function(d) {
@@ -157,60 +154,61 @@ atl_res_patch_speed <- function(data,
             end = as.double(data.table::last(d))
           )
         }), recursive = FALSE), .SDcols = c("x", "y", "time")]
-
+        
         setnames(
           dt2,
           stringr::str_replace(colnames(dt2), "\\.", "_")
         )
-
+        
         return(dt2)
       })]
-
+      
       # assess independence using summary data
       patch_summary <- data[, unlist(patch_summary, recursive = FALSE),
-        by = list(id, patch)
+                            by = list(id, patch)
       ]
       data[, patch_summary := NULL]
-
+      
       # get time bewteen start of n+1 and end of n
       patch_summary[, time_diff := c(
         Inf,
         as.numeric(time_start[2:length(time_start)] -
-          time_end[seq_len(length(time_end) -
-            1)])
+                     time_end[seq_len(length(time_end) -
+                                        1)])
       )]
       # get spatial difference from last to first point
       patch_summary[, spat_diff :=
-        c(atl_patch_dist(
-          data = patch_summary,
-          x1 = "x_end", x2 = "x_start",
-          y1 = "y_end", y2 = "y_start"
-        ))]
+                      c(atl_patch_dist(
+                        data = patch_summary,
+                        x1 = "x_end", x2 = "x_start",
+                        y1 = "y_end", y2 = "y_start"
+                      ))]
+      
       # set spat_diff 1 to Inf
       patch_summary[1, "spat_diff"] <- Inf
-
+      
       # assess independence and assign new patch
       patch_summary[, newpatch := cumsum((time_diff > lim_time_indep) |
-        (spat_diff > lim_spat_indep))]
-
+                                           (spat_diff > lim_spat_indep))]
+      
       # get cols with old and new patch
       patch_summary <- patch_summary[, list(patch, newpatch)]
-
+      
       # basic patch metrics for new patches
       # join patchdata to patch summary by new patch
       # expand data to prepare for new patches
       data <- data[, unlist(patchdata, recursive = FALSE),
-        by = list(id, patch)
+                   by = list(id, patch)
       ]
-
+      
       data <- data.table::merge.data.table(data, patch_summary, by = "patch")
       data[, `:=`(patch = newpatch, newpatch = NULL)]
-
+      
       # nest data again
       data <- data[, list(list(.SD)), by = .(id, patch)]
       setnames(data, old = "V1", new = "patchdata")
       data[, nfixes := as.integer(lapply(patchdata, nrow))]
-
+      
       # basic metrics by new patch
       data[, patch_summary := lapply(patchdata, function(dt) {
         # get mandatory metrics
@@ -221,20 +219,20 @@ atl_res_patch_speed <- function(data,
             end = as.double(data.table::last(d))
           )
         }), recursive = FALSE), .SDcols = c("x", "y", "time")]
-
+        
         setnames(
           dt2,
           stringr::str_replace(colnames(dt2), "\\.", "_")
         )
-
+        
         # now get optional metrics if any asked
         if (length(summary_variables) > 0) {
           dt3 <- data.table::dcast(dt, 1 ~ 1,
-            fun.aggregate = eval(lapply(
-              summary_functions,
-              as.symbol
-            )),
-            value.var = summary_variables
+                                   fun.aggregate = eval(lapply(
+                                     summary_functions,
+                                     as.symbol
+                                   )),
+                                   value.var = summary_variables
           )
           # remove this vestigial column
           dt3[, `.` := NULL]
@@ -248,10 +246,10 @@ atl_res_patch_speed <- function(data,
       data[, dist_in_patch := as.double(lapply(patchdata, function(df) {
         sum(atlastools::atl_simple_dist(data = df), na.rm = TRUE)
       }))]
-
+      
       # distance between patches
       temp_data <- data[, unlist(patch_summary, recursive = FALSE),
-        by = list(id, patch)
+                        by = list(id, patch)
       ]
       data[, patch_summary := NULL]
       data[, dist_bw_patch := atl_patch_dist(
@@ -262,10 +260,10 @@ atl_res_patch_speed <- function(data,
       # displacement in a patch
       # apply func bw patch dist reversing usual end and begin
       temp_data[, disp_in_patch := sqrt((x_end - x_start)^2 +
-        (y_end - y_start)^2)]
+                                          (y_end - y_start)^2)]
       # even more advanced metrics
       temp_data[, duration := (time_end - time_start)]
-
+      
       # true spatial metrics
       data[, polygons := lapply(patchdata, function(df) {
         p1 <- sf::st_as_sf(df, coords = c("x", "y"))
@@ -273,38 +271,36 @@ atl_res_patch_speed <- function(data,
         p2 <- sf::st_union(p2)
         return(p2)
       })]
-
+      
       # add area and circularity
-      data[, area := as.double(lapply(polygons, sf::st_area))]
-      data[, `:=`(circularity = (4 * pi * area) / as.double(lapply(
-        polygons,
-        function(pgon) {
-          boundary <- sf::st_boundary(pgon)
-          perimeter <- sf::st_length(boundary)
-          return(as.numeric(perimeter)^2)
-        }
-      )))]
-
+      #data[, area := as.double(lapply(polygons, sf::st_area))]
+      # data[, `:=`(circularity = (4 * pi * area) / as.double(lapply(
+      #   polygons,
+      #   function(pgon) {
+      #     boundary <- sf::st_boundary(pgon)
+      #     perimeter <- sf::st_length(boundary)
+      #     return(as.numeric(perimeter)^2)
+      #   }
+      #)))]
+      
       # remove polygons
       data[, polygons := NULL]
-
+      
       # remove patch summary from some data and add temp data, then del tempdata
       data <- data.table::merge.data.table(data, temp_data,
-        by = c("id", "patch")
+                                           by = c("id", "patch")
       )
-
+      
       assertthat::assert_that(!is.null(data),
-        msg = "make_patch: patch has no data"
+                              msg = "make_patch: patch has no data"
       )
-
+      
       return(data)
     },
     # null error function, with option to collect data on errors
     error = function(e) {
       message(glue::glue("there was an error in {unique(data$id)}:
-                       {as.character(e)}"))
-    }
-  )
+                         {as.character(e)}"))})
 }
 
 # ends here
